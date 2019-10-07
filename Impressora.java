ba9504a546c44f1534aff45e1c1fb69e2500978d
Victor Henrique Amaral <54120724+victorhadn@users.noreply.github.com>
2019-10-07 20:24:27 -0300

    Add files via upload

---
+++ Impressora.java
@@ -0,0 +1,25 @@
+package visão;
+
+import modelo.Impressao;
+
+public class Impressora extends Impressao{
+	}
+	private Impressao impressao;
+
+	
+	public void exibirImpressao () {
+	
+	
+	 Object tipo;
+	if(tipo.equals("Tela") ){		 
+	 } 
+	 
+     else if(tipo.equals("ArquivoTxt") ){
+	 }
+	
+	 impressao.Tela();
+	 impressao.ArquivoTxt();
+	 
+	
+	
+	
